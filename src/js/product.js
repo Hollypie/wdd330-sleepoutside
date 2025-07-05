a04e1cf66e33cd8ade27877e5ec427f2953ec1d6
TheKingPreto <lucasoliveirasilva2008@gmail.com>
2025-07-05 19:39:52 -0300

    updating the product.js

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,10 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  let cartItems = getLocalStorage('so-cart') || [];
+  let cartItems = getLocalStorage("so-cart") || [];
   cartItems.push(product);
   setLocalStorage("so-cart", cartItems);
 }
