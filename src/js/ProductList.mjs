483ced81f15e9b0b6a23e6398807f1d845360d8c
RayVenShellhart <rayvenshellhart@gmail.com>
2025-07-11 14:04:45 -0700

    individual-rks-2

---
+++ src/js/ProductList.mjs
@@ -0,0 +1,32 @@
+import { renderListWithTemplate } from "./utils.mjs";
+
+function prodcutCardTemplate(product) {
+    return `
+    <li class="product-card">
+        <a href="product_pages/?products=${product.Id}">
+            <img src="${product.Image}" alt="${product.name}">
+            <h2>${product.Brand.Name}</h2>
+            <h3>${product.Name}</h3>
+            <p class"product-card__price">$${product.FinalPrice}</p>
+        </a>
+    </li>
+    `;
+}
+
+export default class ProductList {
+    constructor(category, dataSource, listElement) {
+        this.category = category;
+        this.dataSource = dataSource;
+        this.listElement = listElement;
+    }
+
+    async init() {
+        const list = await this.dataSource.getData();
+        this.renderList(list);
+    }
+
+    renderList(list) {
+        renderListWithTemplate(prodcutCardTemplate, this.listElement, list);
+    }
+
+}
