4cf27388b2d12e04c2744ef8e0e581ddbbbaebbf
TheKingPreto <lucasoliveirasilva2008@gmail.com>
2025-07-11 23:29:24 -0300

    created an empty cart message

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,7 +1,14 @@
 import { getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
+  const cartItems = getLocalStorage("so-cart") || [];
+
+  if (cartItems.length === 0) {
+    document.querySelector(".product-list").innerHTML =
+      "<p class='cart-empty-message'>Your cart is empty.</p>";
+    return;
+  }
+
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
 }
